2cddd7e76003646e193eb25ac86c137727c560f8
faria-karim-porna <faria.porna.08@gmail.com>
2023-05-10 04:39:10 +0600

    router added

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/user" element={<Users />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
