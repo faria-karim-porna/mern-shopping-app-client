099f3387244b30b8e07f8354313cc16015d814c2
faria-karim-porna <faria.porna.08@gmail.com>
2023-05-10 09:18:32 +0000

    Almost Done Login

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -8,38 +8,35 @@ const LoginComponent = () => {
       <div className="d-flex justify-content-center w-100">
         {/* <!-- login background start --> */}
         <div className="login-view">
-          
-            <div className="sub-section-name pl-4 text-center">RockerZZ</div>
-            {/* <!-- login area start --> */}
-            <div className="w-100 pt-2 pb-2 pr-2 pl-2">
-              <div className="login-form-card p-1">
-                <div className="d-flex justify-content-center align-items-center text-center pt-4 pb-4">
-                  <div className="w-75">
-                    {/* <!-- form start --> */}
-
-                    <div className="form-name">Log In</div>
-                    <div className="d-flex justify-content-center">
-                      <div className="w-100">
-                        <input type="email" placeholder="Email" className="w-100" name="Email" />
-                        <input type="password" placeholder="Password" className="w-100" name="Password" />
-                        <div className="mb-4 mt-5">
-                          <button onClick={() => login()}>Log In</button>
-                          <input type="submit" value="Log In" className="w-100" />
-                        </div>
+          <div className="sub-section-name pl-4 text-center">Shopping App</div>
+          {/* <!-- login area start --> */}
+          <div className="w-100 pt-2 pb-2 pr-2 pl-2">
+            <div className="login-form-card p-1">
+              <div className="d-flex justify-content-center align-items-center text-center pt-4 pb-4">
+                <div className="w-75">
+                  {/* <!-- form start --> */}
+                  <div className="form-name">Log In</div>
+                  <div className="d-flex justify-content-center">
+                    <div className="w-100">
+                      <input type="email" placeholder="Email" className="w-100 input-field my-2" name="Email" />
+                      <input type="password" placeholder="Password" className="w-100 input-field my-2" name="Password" />
+                      <div className="mb-4 mt-5">
+                        <button onClick={() => login()} className="log-in-button w-100">
+                          Log In
+                        </button>
                       </div>
                     </div>
-
-                    {/* <!-- form end --> */}
-                    <p className="login-text text-center">
-                      Don't Have An Account?
-                      <span className="login-link-text">Create One</span>
-                    </p>
                   </div>
+                  {/* <!-- form end --> */}
+                  <p className="login-text text-center">
+                    Don't Have An Account?
+                    <span className="login-link-text">Create One</span>
+                  </p>
                 </div>
               </div>
             </div>
-            {/* <!-- login area end --> */}
-          
+          </div>
+          {/* <!-- login area end --> */}
         </div>
         {/* <!-- login background end --> */}
       </div>
