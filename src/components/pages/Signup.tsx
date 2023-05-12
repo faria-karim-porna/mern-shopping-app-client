404eee23667906c6e9aaf87a41eb785c91c8bf42
faria-karim-porna <faria.porna.08@gmail.com>
2023-05-12 22:18:51 +0000

    Updated Sign Up Styling Except Checkbox

--- src/components/pages/Signup.tsx
+++ src/components/pages/Signup.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useAuthentication } from "../hooks/useAuthentication";
+import { GlassmorphismBackground } from "../common/GlassmorphismBackground";
 
 const SignUpComponent = () => {
   const {
@@ -38,109 +39,116 @@ const SignUpComponent = () => {
     }
   };
   return (
-    <div className="page d-flex justify-content-center align-items-center">
-      {/* <!-- sign up section start --> */}
-      <div className="d-flex justify-content-center w-100">
-        {/* <!-- sign up background start --> */}
-        <div className="form-view">
-          <div className="sub-section-name pl-4 text-center">Shopping App</div>
-          {/* <!-- sign up area start --> */}
-          <div className="w-100 pt-2 pb-2 pr-2 pl-2">
-            <div className="form-card p-1">
-              <div className="d-flex justify-content-center align-items-center pt-4 pb-4">
-                <div className="w-75">
-                  {/* <!-- form start --> */}
-                  <div className="form-name text-center">Sign Up</div>
-                  <div className="d-flex justify-content-center">
-                    <div className="w-100">
-                      <input
-                        type="text"
-                        placeholder="Name"
-                        className="w-100 input-field my-2"
-                        name="Name"
-                        onBlur={(e: any) => {
-                          checkNameValidation(e.target.value, false);
-                        }}
-                        onFocus={(e: any) => {
-                          checkNameValidation(e.target.value, true);
-                        }}
-                      />
-                      {validationErrors.name ? <small className="text-danger">{validationErrors.name}</small> : null}
-                      <input
-                        type="email"
-                        placeholder="Email"
-                        className="w-100 input-field my-2"
-                        name="Email"
-                        onBlur={(e: any) => {
-                          checkEmailValidation(e.target.value, false);
-                        }}
-                        onFocus={(e: any) => {
-                          checkEmailValidation(e.target.value, true);
-                        }}
-                      />
-                      {validationErrors.email ? <small className="text-danger">{validationErrors.email}</small> : null}
-                      <input
-                        type="password"
-                        placeholder="Password"
-                        className="w-100 input-field my-2"
-                        name="Password"
-                        onBlur={(e: any) => {
-                          checkPasswordValidation(e.target.value, false);
-                        }}
-                        onFocus={(e: any) => {
-                          checkPasswordValidation(e.target.value, true);
-                        }}
-                      />
-                      {validationErrors.password ? <small className="text-danger">{validationErrors.password}</small> : null}
-                      <input
-                        type="password"
-                        placeholder="Confirm Password"
-                        className="w-100 input-field my-2"
-                        name="ConfirmPassword"
-                        onBlur={(e: any) => {
-                          checkConfirmPasswordValidation(e.target.value, false);
-                        }}
-                        onFocus={(e: any) => {
-                          checkConfirmPasswordValidation(e.target.value, true);
-                        }}
-                      />
-                      {validationErrors.confirmPassword ? <small className="text-danger">{validationErrors.confirmPassword}</small> : null}
+    <GlassmorphismBackground>
+      <div className="d-flex justify-content-center align-items-center">
+        {/* <!-- sign up section start --> */}
+        <div className="d-flex justify-content-center w-100">
+          {/* <!-- sign up background start --> */}
+          <div className="form-view">
+            <div className="sub-section-name pl-4 text-center">Shopping App</div>
+            {/* <!-- sign up area start --> */}
+            <div className="w-100 pt-2 pb-2 pr-2 pl-2">
+              <div className="glass-effect p-1">
+                <div className="d-flex justify-content-center align-items-center pt-4 pb-4">
+                  <div className="w-75">
+                    {/* <!-- form start --> */}
+                    <div className="form-name">
+                      <div>Create An Account</div>
+                      <div className="underline"></div>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                      <div className="w-100">
+                        <input
+                          type="text"
+                          placeholder="Name"
+                          className="w-100 glass-effect py-2 px-3 my-2"
+                          name="Name"
+                          onBlur={(e: any) => {
+                            checkNameValidation(e.target.value, false);
+                          }}
+                          onFocus={(e: any) => {
+                            checkNameValidation(e.target.value, true);
+                          }}
+                        />
+                        {validationErrors.name ? <small className="text-danger">{validationErrors.name}</small> : null}
+                        <input
+                          type="email"
+                          placeholder="Email"
+                          className="w-100 glass-effect py-2 px-3 my-2"
+                          name="Email"
+                          onBlur={(e: any) => {
+                            checkEmailValidation(e.target.value, false);
+                          }}
+                          onFocus={(e: any) => {
+                            checkEmailValidation(e.target.value, true);
+                          }}
+                        />
+                        {validationErrors.email ? <small className="text-danger">{validationErrors.email}</small> : null}
+                        <input
+                          type="password"
+                          placeholder="Password"
+                          className="w-100 glass-effect py-2 px-3 my-2"
+                          name="Password"
+                          onBlur={(e: any) => {
+                            checkPasswordValidation(e.target.value, false);
+                          }}
+                          onFocus={(e: any) => {
+                            checkPasswordValidation(e.target.value, true);
+                          }}
+                        />
+                        {validationErrors.password ? <small className="text-danger">{validationErrors.password}</small> : null}
+                        <input
+                          type="password"
+                          placeholder="Confirm Password"
+                          className="w-100 glass-effect py-2 px-3 my-2"
+                          name="ConfirmPassword"
+                          onBlur={(e: any) => {
+                            checkConfirmPasswordValidation(e.target.value, false);
+                          }}
+                          onFocus={(e: any) => {
+                            checkConfirmPasswordValidation(e.target.value, true);
+                          }}
+                        />
+                        {validationErrors.confirmPassword ? (
+                          <small className="text-danger">{validationErrors.confirmPassword}</small>
+                        ) : null}
 
-                      <div className="checkbox-field">
-                        <label className="checkbox-container">
-                          <p className="checkbox-text">
-                            Agree To Our
-                            <span className="link-text">Terms And Conditions</span>
-                          </p>
-                          <input type="checkbox" name="Terms" onChange={() => setTermsAndCondition()} />
-                          <span className="checkmark"></span>
-                        </label>
-                      </div>
-                      {validationErrors.termsAndCondition ? (
-                        <small className="text-danger">{validationErrors.termsAndCondition}</small>
-                      ) : null}
-                      <div className="mb-4 mt-5">
-                        <button onClick={() => signUp()} className="form-button w-100">
-                          Sign Up
-                        </button>
+                        <div className="checkbox-field">
+                          <label className="checkbox-container">
+                            <p className="checkbox-text">
+                              <span>Agree To Our</span>
+                              <span className="link-text">Terms And Conditions</span>
+                            </p>
+                            <input type="checkbox" name="Terms" onChange={() => setTermsAndCondition()} />
+                            <span className="checkmark"></span>
+                          </label>
+                        </div>
+                        {validationErrors.termsAndCondition ? (
+                          <small className="text-danger">{validationErrors.termsAndCondition}</small>
+                        ) : null}
+                        <div className="mb-4 mt-5">
+                          <button onClick={() => signUp()} className="form-button w-100">
+                            Sign Up
+                          </button>
+                        </div>
                       </div>
                     </div>
+                    {/* <!-- form end --> */}
+                    <p>
+                      <span>Already Have An Account? </span>
+                      <span className="link-text">Login Here</span>
+                    </p>
                   </div>
-                  {/* <!-- form end --> */}
-                  <p className="text-center">
-                    Don't Have An Account?
-                    <span className="link-text">Create One</span>
-                  </p>
                 </div>
               </div>
             </div>
+            {/* <!-- sign up area end --> */}
           </div>
-          {/* <!-- sign up area end --> */}
+          {/* <!-- sign up background end --> */}
         </div>
-        {/* <!-- sign up background end --> */}
+        {/* <!-- sign up section end --> */}
       </div>
-      {/* <!-- sign up section end --> */}
-    </div>
+    </GlassmorphismBackground>
   );
 };
 
