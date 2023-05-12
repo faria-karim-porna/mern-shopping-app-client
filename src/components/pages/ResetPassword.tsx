4c32b2c85c38c5b909f68670808db7a275470e05
faria-karim-porna <faria.porna.08@gmail.com>
2023-05-12 22:57:41 +0000

    Fixed Styling For Reset Password

--- src/components/pages/ResetPassword.tsx
+++ src/components/pages/ResetPassword.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { GlassmorphismBackground } from "../common/GlassmorphismBackground";
+import { Link } from "react-router-dom";
 
 const ResetPasswordComponent = () => {
   const [resetPasswordErrors, setResetPasswordErrors] = useState({
@@ -65,80 +67,91 @@ const ResetPasswordComponent = () => {
   };
   const resetPassword = () => {};
   return (
-    <div className="page d-flex justify-content-center align-items-center">
-      {/* <!-- reset password section start --> */}
-      <div className="d-flex justify-content-center w-100">
-        {/* <!-- reset password background start --> */}
-        <div className="form-view">
-          <div className="sub-section-name pl-4 text-center">Shopping App</div>
-          {/* <!-- reset password area start --> */}
-          <div className="w-100 pt-2 pb-2 pr-2 pl-2">
-            <div className="form-card p-1">
-              <div className="d-flex justify-content-center align-items-center pt-4 pb-4">
-                <div className="w-75">
-                  {/* <!-- form start --> */}
-                  <div className="form-name text-center">Reset Password</div>
-                  <div className="d-flex justify-content-center">
-                    <div className="w-100">
-                      <input
-                        type="email"
-                        placeholder="Email"
-                        className="w-100 input-field my-2"
-                        name="Email"
-                        onBlur={(e: any) => {
-                          checkEmailValidation(e.target.value, false);
-                        }}
-                        onFocus={(e: any) => {
-                          checkEmailValidation(e.target.value, true);
-                        }}
-                      />
-                      {resetPasswordErrors.email ? <small className="text-danger">{resetPasswordErrors.email}</small> : null}
-                      <input
-                        type="password"
-                        placeholder="New Password"
-                        className="w-100 input-field my-2"
-                        name="Password"
-                        onBlur={(e: any) => {
-                          checkPasswordValidation(e.target.value, false);
-                        }}
-                        onFocus={(e: any) => {
-                          checkPasswordValidation(e.target.value, true);
-                        }}
-                      />
-                      {resetPasswordErrors.password ? <small className="text-danger">{resetPasswordErrors.password}</small> : null}
-                      <input
-                        type="password"
-                        placeholder="Confirm New Password"
-                        className="w-100 input-field my-2"
-                        name="ConfirmPassword"
-                        onBlur={(e: any) => {
-                          checkConfirmPasswordValidation(e.target.value, false);
-                        }}
-                        onFocus={(e: any) => {
-                          checkConfirmPasswordValidation(e.target.value, true);
-                        }}
-                      />
-                      {resetPasswordErrors.confirmPassword ? (
-                        <small className="text-danger">{resetPasswordErrors.confirmPassword}</small>
-                      ) : null}
-                      <div className="mb-4 mt-5">
-                        <button onClick={() => resetPassword()} className="form-button w-100">
-                          SAVE
-                        </button>
+    <GlassmorphismBackground>
+      <div className="d-flex justify-content-center align-items-center">
+        {/* <!-- reset password section start --> */}
+        <div className="d-flex justify-content-center w-100">
+          {/* <!-- reset password background start --> */}
+          <div className="form-view">
+            <div className="sub-section-name pl-4 text-center">Shopping App</div>
+            {/* <!-- reset password area start --> */}
+            <div className="w-100 pt-2 pb-2 pr-2 pl-2">
+              <div className="form-card p-1">
+                <div className="d-flex justify-content-center align-items-center pt-4 pb-4">
+                  <div className="w-75">
+                    {/* <!-- form start --> */}
+                    <div className="form-name">
+                      <div>Reset Password</div>
+                      <div className="underline"></div>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                      <div className="w-100">
+                        <input
+                          type="email"
+                          placeholder="Email"
+                          className="w-100 glass-effect py-2 px-3 my-2"
+                          name="Email"
+                          onBlur={(e: any) => {
+                            checkEmailValidation(e.target.value, false);
+                          }}
+                          onFocus={(e: any) => {
+                            checkEmailValidation(e.target.value, true);
+                          }}
+                        />
+                        {resetPasswordErrors.email ? <small className="text-danger">{resetPasswordErrors.email}</small> : null}
+                        <input
+                          type="password"
+                          placeholder="New Password"
+                          className="w-100 glass-effect py-2 px-3 my-2"
+                          name="Password"
+                          onBlur={(e: any) => {
+                            checkPasswordValidation(e.target.value, false);
+                          }}
+                          onFocus={(e: any) => {
+                            checkPasswordValidation(e.target.value, true);
+                          }}
+                        />
+                        {resetPasswordErrors.password ? <small className="text-danger">{resetPasswordErrors.password}</small> : null}
+                        <input
+                          type="password"
+                          placeholder="Confirm New Password"
+                          className="w-100 glass-effect py-2 px-3 my-2"
+                          name="ConfirmPassword"
+                          onBlur={(e: any) => {
+                            checkConfirmPasswordValidation(e.target.value, false);
+                          }}
+                          onFocus={(e: any) => {
+                            checkConfirmPasswordValidation(e.target.value, true);
+                          }}
+                        />
+                        {resetPasswordErrors.confirmPassword ? (
+                          <small className="text-danger">{resetPasswordErrors.confirmPassword}</small>
+                        ) : null}
+                        <div>
+                          <span>Go back to </span>
+                          <Link to="/login">
+                            <span className="link-text">login</span>
+                          </Link>
+                        </div>
+                        <div className="mb-4 mt-3">
+                          <button onClick={() => resetPassword()} className="form-button w-100">
+                            Save
+                          </button>
+                        </div>
                       </div>
                     </div>
+                    {/* <!-- form end --> */}
                   </div>
-                  {/* <!-- form end --> */}
                 </div>
               </div>
             </div>
+            {/* <!-- reset password area end --> */}
           </div>
-          {/* <!-- reset password area end --> */}
+          {/* <!-- reset password background end --> */}
         </div>
-        {/* <!-- reset password background end --> */}
+        {/* <!-- reset password section end --> */}
       </div>
-      {/* <!-- reset password section end --> */}
-    </div>
+    </GlassmorphismBackground>
   );
 };
 
